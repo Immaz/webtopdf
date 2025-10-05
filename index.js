da4d0ad747e85bbf6c66c9ec99e9eb142c83d0ff
Immaz <dev.immaz@gmail.com>
2025-10-05 23:09:00 +0500

    added json support

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
 const { convert } = require("./convert");
 const app = express();
-
+app.use(express.json());
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
